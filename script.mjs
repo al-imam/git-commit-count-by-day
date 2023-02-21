ee7e59bfc2c4b75b0843b8f00d2f45f460585c4e
al-imam <alimam01828@gmail.com>
2023-02-22 03:18:48 +0600

    check script run on git repo or not

--- script.mjs
+++ script.mjs
@@ -2,4 +2,9 @@
 import "zx/globals";
 $.verbose = false;
 
-echo(chalk.blue("Hello World"));
+try {
+  await $`git rev-parse --git-dir`;
+  echo(chalk.greenBright("ou're in git repo"));
+} catch (error) {
+  echo(chalk.red("something is wrong!"));
+}
