ddb55cf049c6a3f84f298dd123bc8d506d737f45
al-imam <alimam01828@gmail.com>
2023-02-22 14:37:28 +0600

    try changing implementation

--- script.mjs
+++ script.mjs
@@ -11,42 +11,7 @@ try {
   throw chalk.red(error.stderr);
 }
 
-const { stdout: output } = await $`git log --no-decorate --date=raw`;
-const logs = output
-  .split("commit")
-  .splice(1)
-  .map((l) => l.toLowerCase())
-  .map((l) => {
-    return l
-      .split("\n")
-      .filter(Boolean)
-      .map((n) => n.trim());
-  })
-  .map((log) => {
-    const logArray = {};
-    logArray.id = log[0];
-    const d = log[2].split(":")[1].trim().split(" +")[0];
-    logArray.date = new Date(parseInt(d) * 1000);
-    const a = log[1].split(":")[1].trim().split(" ");
-    logArray.author = {
-      name: a[0],
-      email: a[1],
-    };
-    logArray.message = log[3];
-    return logArray;
-  })
-  .reduce((a, v) => {
-    if (a[v.date.toLocaleDateString()]) {
-      a[v.date.toLocaleDateString()].push(v);
-      return a;
-    }
-    a[v.date.toLocaleDateString()] = [v];
-    return a;
-  }, {});
+const { stdout: output } =
+  await $`git log --format='%H | %aN | %aE | %as | %s'`;
 
-const logCountByDay = [];
-
-Object.entries(logs).forEach((log) => {
-  logCountByDay.push([log[0], log[1].length]);
-  echo(chalk.cyan([log[0], log[1].length]));
-});
+echo(JSON.stringify(output.split("\n"), null, 4));
